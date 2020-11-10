c5b68485649bce046389696720d919b7c062c115
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-11-10 01:01:35 -0500

    landing page set up

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { BreakpointProvider } from "react-socks";
 
+// container imports
+import Landing from "./containers/Landing";
+
 function App() {
     return (
         <BreakpointProvider>
             <div className="app">
                 <Router>
                     <Switch>
-                        <Route/>
+                        <Route exact path="/" component={Landing} />
                         <Route/>
                         <Route/>
                     </Switch>
---
+++ src/containers/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Breakpoint } from "react-socks";
+
+function Landing() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Landing;
